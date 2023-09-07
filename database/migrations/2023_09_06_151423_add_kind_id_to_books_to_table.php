75e15f002ceb7b2dbff019365b7ef0bb464c4673
Tatsuki0623 <yuyuno918@outlook.jp>
2023-09-07 13:54:12 +0900

    booksテーブルの編集

--- database/migrations/2023_09_06_151423_add_kind_id_to_books_to_table.php
+++ database/migrations/2023_09_06_151423_add_kind_id_to_books_to_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('books', function (Blueprint $table) {
-            //
+            $table->foreignId('kind_id')->constrained('kinds')->comment('ジャンルid');
         });
     }
 
