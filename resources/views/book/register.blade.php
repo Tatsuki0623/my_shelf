8b5f176305c1ef1db500fc5b04409cb0c7650806
Tatsuki0623 <yuyuno918@outlook.jp>
2023-10-11 18:29:35 +0900

    見た目の修正

--- resources/views/book/register.blade.php
+++ resources/views/book/register.blade.php
@@ -23,10 +23,11 @@
                                 </div>
                             </div>
                         </div>
-                        
+                        <div class="py-5">
                             <div class="mx-auto rounded-lg bg-yellow-200 shadow-xl">
                                 <h2 class="text-center text-2xl text-stone-500">本の種類</h2>
                             </div>
+                        </div>
                         <center>
                             <select name="book[kind_id]" class="block rounded-lg border border-gray-300 bg-gray-50 text-base text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
                                 @foreach($kinds as $kind)
@@ -59,8 +60,10 @@
                                         <input id="code" name="isbn" type="text" class="rounded border border-gray-300 bg-gray-100 bg-opacity-50 mx-auto px-3 py-1 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200" placeholder="9784から始まるバーコードをスキャンしてください"/>
                                     </div>
                                     <p class="isbn__error" style="color:red">{{$errors->first('book.isbn')}}</p>
-                                    <div class="mx-auto rounded-lg bg-yellow-200 shadow-xl">
-                                        <h2 class="text-center text-2xl text-stone-500">本の種類</h2>
+                                    <div class="py-5">
+                                        <div class="mx-auto rounded-lg bg-yellow-200 shadow-xl">
+                                            <h2 class="text-center text-2xl text-stone-500">本の種類</h2>
+                                        </div>
                                     </div>
                                     <select name="kind_value" class="block rounded-lg border border-gray-300 bg-gray-50 text-base text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
                                         @foreach($kinds as $kind)
